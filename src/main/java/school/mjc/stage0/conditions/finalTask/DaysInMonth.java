53a1a43e5170f0eb3f5d7d2fad457432a6bfe2aa
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-16 19:58:31 +0300

    Update DaysInMonth.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,21 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-                int dayInMonth = 0;
+        int dayInMonth = 0;
         int isItOk = 0;
-        if (year<0){
+        if (year<0 || month>12 || month<1){
             System.out.println("invalid date");
-            System.exit(0);
         }
-        if (month>12 || month<0){
-            System.out.println("invalid date");
-            System.exit(0);
-        }
+        else {
         switch (year){
             case 1904,1908,1912,1916,1920,1924,1928,1932,1936,1940,1944,1948,1952,1956,1960,1964,1968,1972,1976,1980,1984,1988,1992,1996,2000,
-                2004,2008,2012,2016,2020:
+                    2004,2008,2012,2016,2020:
                 isItOk = 1;
-            break;
+                break;
             default:
                 isItOk = 0;
                 break;
@@ -49,5 +45,7 @@ public class DaysInMonth {
         }
         System.out.println(dayInMonth);
 
+
+    }}
     }
 }
